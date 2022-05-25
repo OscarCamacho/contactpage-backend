458758e9af546e2b2d0b4e7ea897f6f32da1acc0
Oscar U. Camacho Hernandez <oscar.u.camacho.hernandez@gmail.com>
2022-05-25 18:48:41 -0500

    Posible fix to cors issue v5

--- src/main/java/mx/com/camacho/contactpage/config/CorsEnvironmentConfiguration.java
+++ src/main/java/mx/com/camacho/contactpage/config/CorsEnvironmentConfiguration.java
@@ -1,5 +1,6 @@
 package mx.com.camacho.contactpage.config;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
@@ -7,12 +8,14 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableWebMvc
+@Slf4j
 public class CorsEnvironmentConfiguration implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("**")
+        registry.addMapping("/**")
                 .allowedMethods("GET")
                 .allowedOrigins("**"); // Maybe insecure
+        log.warn("CORS configured");
     }
 }
